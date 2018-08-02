b11925fa9ed8e957a77b955ecda9784e3aa908ca
Luisa Pichardo <luisapichardo@gmail.com>
2018-08-02 13:58:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Sup"
+  end
+  
+  get '/name' do
+  "My name is Luisa"
+  end
+  
+  get '/hometown' do 
+    "My home town is Miami,FL"
+  end 
+  
+  get '/favorite_song' do 
+    "My favorite song is A team by ed sheeren"
+  end 
 end
+
